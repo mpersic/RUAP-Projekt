80f6e1752be38bcb2bc7a1767c36e1f5582cad05
Matej <Matej@DESKTOP-1AI2CEV>
2021-10-26 22:17:00 +0200

    pozdrav iz aplikacije commit

--- Ruap/Ruap/Program.cs
+++ Ruap/Ruap/Program.cs
@@ -6,11 +6,7 @@ namespace Ruap
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Pozdrav iz aplikacije od Mateja Peršića! Broj indeksa ovdje (kada bi ga imali)");
-=======
             Console.WriteLine("Pozdrav iz aplikacije od Mateja Peršića!");
->>>>>>> b194292 (pozdrav iz aplikacije commit)
         }
     }
 }
